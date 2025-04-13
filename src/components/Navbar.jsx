d670dff09d2b6db6333da70ac17d63f5c21ae369
Shamanth-GiT <shamanth.kuthpadi2003@gmail.com>
2025-04-13 16:48:51 -0400

    resume update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
         </Link>
         <a
           className="navbar-elem"
-          href="https://drive.google.com/file/d/1xLb4kvgFkh25lIZgvurpjbBDnMKbsQoJ/view?usp=sharing"
+          href="https://drive.google.com/file/d/1fn_7BNNuojMudQX1D1wUy1lo7sN56pV6/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
